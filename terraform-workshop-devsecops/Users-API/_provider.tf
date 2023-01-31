42e6572a43a06abd8d5e15b7d097fe6fd6999fd8
Ramesh Patel <rpatel@oak9.io>
2023-01-31 15:13:58 -0600

    first commit for workshops

---
+++ terraform-workshop-devsecops/Users-API/_provider.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0.0"
+    }
+    archive = {
+      source  = "hashicorp/archive"
+      version = "~> 2.2.0"
+    }
+  }
+  required_version = "~> 1.1.9"
+}
+
+provider "aws" {
+  region  = "us-east-2"
+  #needed if you are using aws profiles 
+  profile        = "sandbox"
+}
